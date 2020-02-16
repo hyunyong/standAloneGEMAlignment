54643713946b61bd764bde88c633c9cd2e940434
hyunyong <antilogy33@gmail.com>
2020-02-16 14:40:33 +0100

    add GE2/1

--- makeTxt.py
+++ makeTxt.py
@@ -4,8 +4,10 @@ f = open("gemAl.csv", 'w')
 w = csv.writer(f)
 
 for r in [-1,1]:
-  for s in [1]:
-    for c in range(36):
+  for s in [1,2]:
+    if s == 1: nsec = 36
+    if s == 2: nsec = 18
+    for c in range(nsec):
       detNum = r*(s*100+c+1)
       dx = float("%1.3f"%random.gauss(0,1))
       dy = 0.0
